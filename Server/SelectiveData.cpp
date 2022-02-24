30b57a63dc017c6ed7eda9de42958ee09b93b378
Adarsh08X <adarsh.kumar2@incedoinc.com>
2022-02-24 19:49:28 +0530

    Added query for reference

--- Server/SelectiveData.cpp
+++ Server/SelectiveData.cpp
@@ -4,10 +4,13 @@ void SelectiveData() {
 	stringstream selective;
 	selective << "select * from stats where user_id = 'abc123' order by time_sec desc limit 5;";
 	
+<<<<<<< HEAD
 }
 string verifyUserId(string userId)
 {
 	stringstream ss;
 	ss << "INSERT IGNORE INTO users VALUES ('" << userId << "');";
 	return ss.str();
+=======
+>>>>>>> Added query for reference
 }
