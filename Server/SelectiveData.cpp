6ff95e44ab0c04ef06c3bf48c5c95a701d8364bb
Nitish Kumar <nitish.kumar3@incedoinc.com>
2022-02-25 12:34:06 +0530

    Made primary key and handled verification.

--- Server/SelectiveData.cpp
+++ Server/SelectiveData.cpp
@@ -5,3 +5,9 @@ void SelectiveData() {
 	selective << "select * from stats where user_id = 'abc123' order by time_sec desc limit 5;";
 	
 }
+string verifyUserId(string userId)
+{
+	stringstream ss;
+	ss << "INSERT IGNORE INTO users VALUES ('" << userId << "');";
+	return ss.str();
+}
